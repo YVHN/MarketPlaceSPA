a9010f685a7dae81a9d4b085e4716b0f01379d02
Yevhen Suprunenko <gollandolion@gmail.com>
2024-04-30 21:54:02 +0200

    fix another interfaces styles bug

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/views/MarketPlace/Assets/Styles/Global.scss';
+import '@/views/MarketPlace/Assets/Fonts/PFDinDisplayPro/PFDinDisplayPro.scss';
 
 import './functions/multilanguage'
 // import "./functions/camera"
--- src/views/MarketPlace/MarketPlace.vue
+++ src/views/MarketPlace/MarketPlace.vue
@@ -43,4 +43,4 @@ export default {
 };
 </script>
 
-<style lang="scss" src="./MarketPlace.scss" scoped></style>
+<style lang="scss" src="./MarketPlace.scss"></style>
