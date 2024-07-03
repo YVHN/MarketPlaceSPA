92579305739222d8b7c498f0f3d4551b210f958d
Epsilon Eridani <tsuuzetsu@gmail.com>
2024-07-03 14:59:26 +0300

    update

--- src/views/MarketPlace/Pages/CreateListing/Components/ListingEditor/Components/DeployListing/DeployListing.vue
+++ src/views/MarketPlace/Pages/CreateListing/Components/ListingEditor/Components/DeployListing/DeployListing.vue
@@ -110,7 +110,7 @@ export default {
     },
     getPriceForHours() {
       let basePrice = this.pickedHours * this.$store.getters.getPricePerHour;
-      let percents = this.price * 0.1;
+      let percents = this.price * 0.03;
       return this.formatNumber(basePrice + percents);
     },
   },
